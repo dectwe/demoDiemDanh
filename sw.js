56cf959d686ce6298d1d464152f33946c9ffb026
dectwe <127535669+dectwe@users.noreply.github.com>
2026-07-16 12:31:26 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
-const CACHE_NAME = 'demoDiemDanhv2';
-const urlsToCache = [
+const CACHE_NAME = 'ai-attendance-v4'; // Tăng lên v4 để ép điện thoại xóa cache cũ
+const ASSETS = [
   '/',
   '/index.html',
   '/register.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/face-api-local.js', // Đổi tên file thư viện tại đây
+  
+  '/face-api.js-master/weights/ssd_mobilenetv1_model-weights_manifest.json',
+  '/face-api.js-master/weights/ssd_mobilenetv1_model-shard1',
+  '/face-api.js-master/weights/face_landmark_68_model-weights_manifest.json',
+  '/face-api.js-master/weights/face_landmark_68_model-shard1',
+  '/face-api.js-master/weights/face_recognition_model-weights_manifest.json',
+  '/face-api.js-master/weights/face_recognition_model-shard1'
 ];
 
 self.addEventListener('install', event => {
